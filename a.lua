92256df4b1832355cc46cf41bec35c0c7938f545
HeheBoi1234 <108604181+HeheBoi1234@users.noreply.github.com>
2023-09-10 21:22:28 +0700

    Update a.lua

--- a.lua
+++ a.lua
@@ -1,4 +1,12 @@
+local cc = {}
+function cc.Finished()
+    return true
+end
+function cc.New(a)
+    for i, v in a do print(i,v) end
+end
+function cc.Closed()
+    return true
+end
 
-
-     print("b")
-
+return cc
